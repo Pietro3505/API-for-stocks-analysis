257db193cd298a26229a800f1ef924a8043680f6
Pietro <pmunoz3505@gmail.com>
2023-10-16 17:27:09 +0200

    "Change of file names"

--- index.js
+++ index.js
@@ -6,7 +6,5 @@ const results = require('./routes/results');
 app.use(express.json());
 app.use('/', results);
 
-
-
 let port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on port: ${port}`))
