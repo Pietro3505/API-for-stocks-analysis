0f919692939ee64411c99fba283a4e92a47430cc
Pietro <pmunoz3505@gmail.com>
2023-10-16 15:09:03 +0200

    extra files

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const results = require('./routes/results');
+
+//Middleware
+app.use(express.json());
+app.use('/', results);
+
+
+
+let port = process.env.PORT || 3000
+app.listen(port, () => console.log(`listening on port: ${port}`))
